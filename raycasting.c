1fd9a979a0f9361133eb3205add5ea86748031d6
Gregory HAVENGA <ghavenga@e5r7p4.jnb.42.fr>
2016-06-13 15:47:14 +0200

    raycasting.c

---
+++ raycasting.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   raycasting.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ghavenga <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/06/13 13:58:49 by ghavenga          #+#    #+#             */
+/*   Updated: 2016/06/13 15:46:55 by ghavenga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "wolf3d.h"
+
+void	draw_screen()
+{
+
+	//Draw screen collumn by collumn by determining the players position on the
+	//map and the angle they are faceing. 
+	//(angle is determined by placing a point in front of the the players x y
+	//coordinate that you apply move commands to in the same way they are applied
+	//to the player x y, but with the added ability to rotate the additional point
+	//using the player xy as an origin point.)
+}
+
+void				draw_collumn (t_win *win)
+{
+	unsigned int	k;
+
+	mlx_put_pixel()
+}
