1647eb93dcf4a83aa2bd046bd52497a749cfbf09
Owen EXALL <oexall@e5r6p1.jnb.42.fr>
2016-06-17 07:58:32 +0200

    Added the array to the t_main struct.

--- wolf3d.c
+++ wolf3d.c
@@ -6,7 +6,7 @@
 /*   By: oexall <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/13 09:08:34 by oexall            #+#    #+#             */
-/*   Updated: 2016/06/16 10:05:14 by oexall           ###   ########.fr       */
+/*   Updated: 2016/06/17 07:21:22 by oexall           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,11 @@ int		main(int argc, char **argv)
 	mlx_hook(main.win.win, 3, (1L << 1), &keyrelease, &main);
 	if (!(argc > 1 && argc < 3) || !(argv[1]))
 		ft_puterror("main: Invalid Args");
+
 	main.map = read_map(argv[1]);
+	ft_printf("Reading Array:\n");
+	main.array = get_array(&main.map);
+
 	mlx_loop(main.win.mlx);
 	return (0);
 }
